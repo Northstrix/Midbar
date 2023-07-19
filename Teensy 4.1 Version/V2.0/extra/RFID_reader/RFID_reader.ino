d4edca73467e5db99655d3d855fef3ed88453747
Maxim Bortnikov <43994704+Northstrix@users.noreply.github.com>
2023-07-19 20:47:24 +0700

    Add files via upload

---
+++ Teensy 4.1 Version/V2.0/extra/RFID_reader/RFID_reader.ino
@@ -0,0 +1,65 @@
+/*
+Midbar
+Distributed under the MIT License
+© Copyright Maxim Bortnikov 2023
+For more information please visit
+https://sourceforge.net/projects/midbar/
+https://osdn.net/projects/midbar/
+https://github.com/Northstrix/Midbar
+Required libraries:
+https://github.com/zhouyangchao/AES
+https://github.com/peterferrie/serpent
+https://github.com/ddokkaebi/Blowfish
+https://github.com/Northstrix/DES_and_3DES_Library_for_MCUs
+https://github.com/ulwanski/sha512
+https://github.com/adafruit/Adafruit_ILI9341
+https://github.com/adafruit/Adafruit-GFX-Library
+https://github.com/adafruit/Adafruit_BusIO
+https://github.com/intrbiz/arduino-crypto
+https://github.com/GyverLibs/EncButton
+https://github.com/miguelbalboa/rfid
+*/
+#include <SPI.h>
+#include <MFRC522.h>
+
+/*
+ * SDA - 25
+ * SCK - 13
+ * MOSI - 11
+ * MISO - 12
+ * RST - 24
+ */
+
+#define SS_PIN  25
+#define RST_PIN 24
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(115200);
+  SPI.begin(); // init SPI bus
+  rfid.PCD_Init(); // init MFRC522
+
+  Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader");
+}
+
+void loop() {
+  if (rfid.PICC_IsNewCardPresent()) { // new tag is available
+    if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      Serial.print("RFID/NFC Tag Type: ");
+      Serial.println(rfid.PICC_GetTypeName(piccType));
+
+      // print UID in Serial Monitor in the hex format
+      Serial.print("UID:");
+      for (int i = 0; i < rfid.uid.size; i++) {
+        Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(rfid.uid.uidByte[i], HEX);
+      }
+      Serial.println();
+
+      rfid.PICC_HaltA(); // halt PICC
+      rfid.PCD_StopCrypto1(); // stop encryption on PCD
+    }
+  }
+}

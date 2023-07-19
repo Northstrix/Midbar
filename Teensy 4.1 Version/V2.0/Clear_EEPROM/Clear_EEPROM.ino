08caee0190e2d68b7df64bb0cb2c2b94af3d675f
Maxim Bortnikov <43994704+Northstrix@users.noreply.github.com>
2023-07-19 20:38:16 +0700

    Add files via upload

---
+++ Teensy 4.1 Version/V2.0/Clear_EEPROM/Clear_EEPROM.ino
@@ -0,0 +1,31 @@
+/*
+Midbar
+Distributed under the MIT License
+© Copyright Maxim Bortnikov 2023
+For more information please visit
+https://sourceforge.net/projects/midbar/
+https://osdn.net/projects/midbar/
+https://github.com/Northstrix/Midbar
+Required libraries:
+https://github.com/zhouyangchao/AES
+https://github.com/peterferrie/serpent
+https://github.com/ddokkaebi/Blowfish
+https://github.com/Northstrix/DES_and_3DES_Library_for_MCUs
+https://github.com/ulwanski/sha512
+https://github.com/adafruit/Adafruit_ILI9341
+https://github.com/adafruit/Adafruit-GFX-Library
+https://github.com/adafruit/Adafruit_BusIO
+https://github.com/intrbiz/arduino-crypto
+https://github.com/GyverLibs/EncButton
+https://github.com/miguelbalboa/rfid
+*/
+#include <EEPROM.h>
+
+void setup()
+{
+  for (unsigned int i = 0; i < EEPROM.length(); i++ )
+    EEPROM.write(i, 0);
+
+}
+
+void loop() { } 

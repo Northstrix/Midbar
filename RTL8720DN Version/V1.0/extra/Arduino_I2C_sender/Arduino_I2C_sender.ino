47a66b803363fc30976503b240f4658d108179ff
Maxim Bortnikov <43994704+Northstrix@users.noreply.github.com>
2023-08-01 20:57:26 +0700

    Add files via upload

---
+++ RTL8720DN Version/V1.0/extra/Arduino_I2C_sender/Arduino_I2C_sender.ino
@@ -0,0 +1,52 @@
+/*
+Midbar
+Distributed under the MIT License
+© Copyright Maxim Bortnikov 2023
+For more information please visit
+https://sourceforge.net/projects/midbar/
+https://osdn.net/projects/midbar/
+https://github.com/Northstrix/Midbar
+Required libraries:
+https://github.com/zhouyangchao/AES
+https://github.com/ddokkaebi/Blowfish
+https://github.com/intrbiz/arduino-crypto
+https://github.com/GyverLibs/EncButton
+https://github.com/techpaul/PS2KeyAdvanced
+https://github.com/techpaul/PS2KeyMap
+*/
+// Wire Master Writer
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Writes data to an I2C/TWI slave device
+// Refer to the "Wire Slave Receiver" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+/*
+
+ Example guide:
+ https://www.amebaiot.com/en/amebad-arduino-i2c-1/
+ */
+
+#include <Wire.h>
+
+void setup() {
+    Serial.begin(115200);
+    Wire.begin(); // join i2c bus (address optional for master)
+}
+
+byte x = 0;
+
+void loop() {
+    Wire.beginTransmission(13);  // transmit to device #87
+    Wire.write(x);              // sends one byte
+    Wire.endTransmission();     // stop transmitting
+
+    x++;
+    if (x == 256)
+      x = 0;
+    delay(4);
+}
